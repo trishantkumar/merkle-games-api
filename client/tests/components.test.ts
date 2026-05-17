c8d76a0e0b3dc3aa15307535fcb6acecc17c181b
Trishant Kumar <trishantshishodia@gmail.com>
2026-05-17 14:09:22 +0530

    bug: update Loader path in component test file

--- client/tests/components.test.ts
+++ client/tests/components.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import { mountSuspended } from "@nuxt/test-utils/runtime";
-import Loader from "../app/components/Atoms/Loader.vue";
+import Loader from "../app/components/Molecules/Loader.vue";
 import GamesCard from "../app/components/Organisms/GamesCard.vue";
 
 describe("test components rendering", () => {
